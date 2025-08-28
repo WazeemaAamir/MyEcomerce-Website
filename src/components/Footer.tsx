dd931876e053637d18065f69c44973d98d721a7f
WAZEEMA AAMIR <161026876+WazeemaAamir@users.noreply.github.com>
2025-08-28 23:46:56 +0500

    Delete src directory

--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-100 text-center p-4 mt-8">
-      <p className="text-gray-500">© 2025 MyStore. All rights reserved.</p>
-    </footer>
-  );
-}
